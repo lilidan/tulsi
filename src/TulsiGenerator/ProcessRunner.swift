e03b6430981bf961bc64ff5d4f35e5db096001d8
Jerry Marino <i@jerrymarino.com>
2019-01-17 17:19:18 -0800

    Fix Swift 4.2 issues
    Change-Id: Iaf0105a43d984a1eabcb18ae62186aec4271f4da

--- src/TulsiGenerator/ProcessRunner.swift
+++ src/TulsiGenerator/ProcessRunner.swift
@@ -307,10 +307,10 @@ public final class ProcessRunner {
     private func threadMain(_ object: AnyObject) {
       let runLoop = RunLoop.current
       // Add a dummy port to prevent the runloop from returning immediately.
-      runLoop.add(NSMachPort(), forMode: RunLoop.Mode.default)
+      runLoop.add(NSMachPort(), forMode: RunLoopMode.defaultRunLoopMode)
 
       while !thread.isCancelled {
-        runLoop.run(mode: RunLoop.Mode.default, before: Date.distantFuture)
+        runLoop.run(mode: RunLoopMode.defaultRunLoopMode, before: Date.distantFuture)
       }
     }
 
@@ -320,3 +320,4 @@ public final class ProcessRunner {
     }
   }
 }
+
